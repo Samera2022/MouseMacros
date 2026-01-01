d603e956dbb10f68f899e75442f713f148972700
Samera2022 <SamCream@126.com>
2026-01-01 13:11:49 +0800

    1.1.0

--- src/io/github/samera2022/mouse_macros/UpdateInfo.java
+++ src/io/github/samera2022/mouse_macros/UpdateInfo.java
@@ -87,7 +87,7 @@ public enum UpdateInfo {
     VERSION_1_1_0("1.1.0","2026-01-01 12:52",
             "## [Info]\n" +
                     " - 从此版本开始，将严格按照语义化版本（SemVer）重新编写版号。\n" +
-                    " - 在以往版本中，通常采用第三位表示小功能更新，第二位表示重构功能更新，第一位表示重大更新。现在将采用第三位表示修订更新，第二位表示次版本更新，第一位表示主版本更新。\n\n" +
+                    " - 在以往版本中，通常采用第三位表示小功能更新，第二位表示重构更新，第一位表示重大更新。现在将采用第三位表示修订更新，第二位表示次版本更新，第一位表示主版本更新。\n\n" +
                     "## [Fixed]\n" +
                     " - 修复了1.0.4中的Tooltip长文本显示相关逻辑，使之更加准确地生效。");
 
@@ -130,6 +130,6 @@ public enum UpdateInfo {
     }
 
     public static void main(String[] args) {
-        System.out.println(VERSION_1_0_4.getFormattedLog());
+        System.out.println(VERSION_1_1_0.getFormattedLog());
     }
 }
